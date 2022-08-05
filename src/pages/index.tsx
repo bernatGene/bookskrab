91819a2f40d0e6e71f75cdc3e40bbdf8a7978522
Bernat Skrabec <bernat.skrabec@marss.com>
2022-08-05 20:49:11 +0200

    no profanity

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div className="text-2xl text-red-200"> Hello bitch, puta</div>
+  return <div className="text-2xl text-red-200"> Hello, something nicer</div>
 } 
