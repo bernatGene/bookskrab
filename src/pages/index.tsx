f426309324ac7c3caf6f403d8e9f163b5387145a
Bernat Skrabec <bernat.skrabec@marss.com>
2022-08-06 16:56:21 +0200

    going for the book app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <div className="text-2xl text-red-200"> Hello, something nicer</div>
+  return (
+    <div className="h-screen w-screen flex flex-col justify-start items-center"> 
+      <div className="p-8"></div>
+      <div className="text-xl text-center">Book title</div>
+      <div className="p-2"></div>
+      <div className="border rounded p-16 flex max-w-2xl text-center">Book info</div>
+    </div>
+  );
 } 
