68c99b673331e7a20eb71047093811d0dd595c45
Bernat Skrabec <bernat.skrabec@marss.com>
2022-08-07 14:11:19 +0200

    trpc test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,10 @@
+import { trpc } from "../utils/trpc"
+
+
 export default function Home() {
+  const {data, isLoading} = trpc.useQuery(["example.hello", {text: "Bernat"}])
+  if (isLoading) return <div>is Loading</div>
+  if (data) return <div>{data.greeting}</div>
   return (
     <div className="h-screen w-screen flex flex-col justify-start items-center"> 
       <div className="p-8"></div>
