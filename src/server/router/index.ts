52d028d3f9a3ac6b887df1a24bb075000b7b3ed0
Bernat Skrabec <bernatgene@gmail.com>
2022-08-13 02:20:39 +0200

    deleted example router

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,14 +2,10 @@
 import { createRouter } from "./context";
 import superjson from "superjson";
 
-import { exampleRouter } from "./example";
-import { protectedExampleRouter } from "./protected-example-router";
 import { bookRouter } from "./bookinfo";
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("example.", exampleRouter)
-  .merge("question.", protectedExampleRouter)
   .merge("book.", bookRouter);
 
 // export type definition of API
